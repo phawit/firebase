7f5f3cd86ba900d44d8338c6cecece8e8199dd37
phawit <firstboonrat@gmail.com>
2018-04-22 07:30:24 +0700

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCvu4OzAWO5HX5N0dB_jG97xbv8uiHdhyk",
+    authDomain: "fusion-chart-demo.firebaseapp.com",
+    databaseURL: "https://fusion-chart-demo.firebaseio.com",
+    projectId: "fusion-chart-demo",
+    storageBucket: "",
+    messagingSenderId: "1061154162455"
+  };
+  firebase.initializeApp(config);
